2a65dc6a9c633d30f9f6ecf8eaace924d1bff2bb
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-11 18:26:46 +0200

    Add the CityRepository

---
+++ src/main/java/com/dauphine/my_trip/repositories/CityRepository.java
@@ -0,0 +1,11 @@
+package com.dauphine.my_trip.repositories;
+
+import com.dauphine.my_trip.models.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CityRepository extends JpaRepository<City, UUID> {
+    Optional<City> findByName(String cityName);
+}

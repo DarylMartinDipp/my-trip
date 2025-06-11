c39206fa47dea3c0dea16bb09522f04b8d639fe3
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-11 18:27:15 +0200

    Adjust for Repository
    Adjust the code to include the new CityRepository

--- src/main/java/com/dauphine/my_trip/services/CityService.java
+++ src/main/java/com/dauphine/my_trip/services/CityService.java
@@ -1,19 +1,24 @@
 package com.dauphine.my_trip.services;
 
+import com.dauphine.my_trip.exceptions.city.CityNameAlreadyExistsException;
+import com.dauphine.my_trip.exceptions.city.CityNotFoundByIdException;
 import com.dauphine.my_trip.models.City;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CityService {
 
     List<City> getAllCities();
 
-    City getCityById(UUID id);
+    City getCityById(UUID id) throws CityNotFoundByIdException;
 
-    City createCity(String newCityName, String newCityCountry);
+    Optional<City> getCityByName(String name);
 
-    City updateCity(UUID cityId, String newCityName, String newCityCountry);
+    City createCity(String newCityName, String newCityCountry) throws CityNameAlreadyExistsException;
 
-    void deleteCity(UUID cityToDeleteId);
+    City updateCity(UUID cityId, String newCityName, String newCityCountry) throws CityNotFoundByIdException, CityNameAlreadyExistsException;
+
+    void deleteCity(UUID cityToDeleteId) throws CityNotFoundByIdException;
 }
--- src/main/java/com/dauphine/my_trip/services/impl/CityServiceImpl.java
+++ src/main/java/com/dauphine/my_trip/services/impl/CityServiceImpl.java
@@ -1,59 +1,65 @@
 package com.dauphine.my_trip.services.impl;
 
+import com.dauphine.my_trip.exceptions.city.CityNameAlreadyExistsException;
+import com.dauphine.my_trip.exceptions.city.CityNotFoundByIdException;
 import com.dauphine.my_trip.models.City;
+import com.dauphine.my_trip.repositories.CityRepository;
 import com.dauphine.my_trip.services.CityService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class CityServiceImpl implements CityService {
-    private final List<City> temporariesCities;
+    private final CityRepository cityRepository;
 
-    public CityServiceImpl(List<City> temporariesCities) {
-        this.temporariesCities = temporariesCities;
-        temporariesCities.add(new City("Paris", "France"));
-        temporariesCities.add(new City("Nice", "France"));
-        temporariesCities.add(new City("London", "England"));
-        temporariesCities.add(new City("Abidjan", "Ivory Coast"));
-        temporariesCities.add(new City("Alger", "Algeria"));
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
     }
 
     @Override
     public List<City> getAllCities() {
-        return temporariesCities;
+        return cityRepository.findAll();
     }
 
     @Override
-    public City getCityById(UUID id) {
-        return temporariesCities.stream()
-                .filter(city -> id.equals(city.getId()))
-                .findFirst()
-                .orElse(null);
+    public City getCityById(UUID cityId) throws CityNotFoundByIdException {
+        return cityRepository.findById(cityId).orElseThrow(() -> new CityNotFoundByIdException(cityId));
     }
 
     @Override
-    public City createCity(String newCityName, String newCityCountry) {
+    public Optional<City> getCityByName(String cityName){
+        return cityRepository.findByName(cityName);
+    }
+
+    @Override
+    public City createCity(String newCityName, String newCityCountry) throws CityNameAlreadyExistsException {
+        Optional<City> existingCityByName = cityRepository.findByName(newCityName);
+        if (existingCityByName.isPresent()) throw new CityNameAlreadyExistsException(newCityName);
+
         City newCity = new City(newCityName, newCityCountry);
-        temporariesCities.add(newCity);
-        return newCity;
+        return cityRepository.save(newCity);
     }
 
     @Override
-    public City updateCity(UUID cityId, String newCityName, String newCityCountry) {
+    public City updateCity(UUID cityId, String newCityName, String newCityCountry) throws CityNotFoundByIdException, CityNameAlreadyExistsException {
         City cityToUpdate = getCityById(cityId);
 
-        if (cityToUpdate != null) {
-            cityToUpdate.setName(newCityName);
-            cityToUpdate.setCountry(newCityCountry);
-        }
+        Optional<City> existingCityByName = cityRepository.findByName(newCityName);
+        if (existingCityByName.isPresent() && !existingCityByName.get().equals(cityToUpdate))
+            throw new CityNameAlreadyExistsException(newCityName);
 
-        return cityToUpdate;
+        cityToUpdate.setName(newCityName);
+        cityToUpdate.setCountry(newCityCountry);
+
+        return cityRepository.save(cityToUpdate);
     }
 
     @Override
-    public void deleteCity(UUID cityToDeleteId) {
-        temporariesCities.removeIf(city -> city.getId().equals(cityToDeleteId));
+    public void deleteCity(UUID cityToDeleteId) throws CityNotFoundByIdException {
+        getCityById(cityToDeleteId);
+        cityRepository.deleteById(cityToDeleteId);
     }
 }

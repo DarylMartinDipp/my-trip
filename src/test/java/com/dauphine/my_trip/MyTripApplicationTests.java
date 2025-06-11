03bcfafdb9b9b58d77aff5eecda5a04848f96b41
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-11 14:37:28 +0200

    Initial commit

---
+++ src/test/java/com/dauphine/my_trip/MyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.my_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

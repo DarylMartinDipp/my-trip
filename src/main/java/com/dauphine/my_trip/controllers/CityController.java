ed7a423b0edd95db3a9c78066c83831a2955ddbf
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-13 12:03:49 +0200

    Add ResponseEntity
    Add ResponseEntity into CityController

--- src/main/java/com/dauphine/my_trip/controllers/CityController.java
+++ src/main/java/com/dauphine/my_trip/controllers/CityController.java
@@ -1,11 +1,16 @@
 package com.dauphine.my_trip.controllers;
 
+import com.dauphine.my_trip.dto.CreateCityRequest;
+import com.dauphine.my_trip.exceptions.city.CityNameAlreadyExistsException;
+import com.dauphine.my_trip.exceptions.city.CityNotFoundByIdException;
 import com.dauphine.my_trip.models.City;
 import com.dauphine.my_trip.services.CityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
@@ -29,10 +34,11 @@ public class CityController {
             description = "Return all cities that are in the database, sorted " +
                     "by alphabetical order."
     )
-    public List<City> getAllCities() {
-        List <City> citiesToGet = cityService.getAllCities();
+    public ResponseEntity<List<City>> getAllCities(@RequestParam (required = false) String cityName) {
+        List<City> citiesToGet = cityName == null || cityName.isBlank() ?
+                cityService.getAllCities() : cityService.getCityByNameIgnoreCase(cityName);
         citiesToGet.sort(Comparator.comparing(City::getName));
-        return citiesToGet;
+        return ResponseEntity.ok(citiesToGet);
     }
 
     @GetMapping("/{cityId}")
@@ -40,8 +46,13 @@ public class CityController {
             summary = "Get a city by ID endpoint",
             description = "Return a certain city according to its id."
     )
-    public City getCityById(@PathVariable UUID cityId) {
-        return cityService.getCityById(cityId);
+    public ResponseEntity<City> getCityById(@PathVariable UUID cityId) {
+        try {
+            final City cityToGet = cityService.getCityById(cityId);
+            return ResponseEntity.ok(cityToGet);
+        } catch (CityNotFoundByIdException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
@@ -49,8 +60,17 @@ public class CityController {
             summary = "Create a new city endpoint",
             description = "Create a new city with all its data."
     )
-    public City createCity(@RequestBody String name, @RequestBody String country) {
-        return cityService.createCity(name, country);
+    public ResponseEntity<City> createCity(@RequestBody CreateCityRequest cityToCreate) {
+        try {
+            City city = cityService.createCity(
+                    cityToCreate.getName(), cityToCreate.getCountry()
+            );
+            return ResponseEntity
+                    .created(URI.create("cities/" + city.getId()))
+                    .body(city);
+        } catch (CityNameAlreadyExistsException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping("/{cityId}")
@@ -58,8 +78,17 @@ public class CityController {
             summary = "Update a city endpoint",
             description = "Update a city according to the id."
     )
-    public City updateCity(@PathVariable UUID cityId, @RequestBody String name, @RequestBody String country) {
-        return cityService.updateCity(cityId, name, country);
+    public ResponseEntity<City> updateCity(@PathVariable UUID cityId, @RequestBody CreateCityRequest cityToUpdate) {
+        try {
+            City user = cityService.updateCity(
+                    cityId, cityToUpdate.getName(), cityToUpdate.getCountry()
+            );
+            return ResponseEntity.ok(user);
+        } catch (CityNotFoundByIdException e) {
+            return ResponseEntity.notFound().build();
+        } catch (CityNameAlreadyExistsException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @DeleteMapping("/{cityId}")
@@ -67,7 +96,12 @@ public class CityController {
             summary = "Delete a city endpoint",
             description = "Delete an existing city according to the id."
     )
-    public void deleteCity(@PathVariable UUID cityId) {
-        cityService.deleteCity(cityId);
+    public ResponseEntity<Void> deleteCity(@PathVariable UUID cityId) {
+        try {
+            cityService.deleteCity(cityId);
+            return ResponseEntity.ok().build();
+        } catch (CityNotFoundByIdException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }

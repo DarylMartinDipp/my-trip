5cc0fd1d6f24205b21e205d139be3159d3700abd
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-13 12:04:20 +0200

    Add findByNameContainingIgnoreCase
    Add findByNameContainingIgnoreCase with the SQL query

--- src/main/java/com/dauphine/my_trip/repositories/CityRepository.java
+++ src/main/java/com/dauphine/my_trip/repositories/CityRepository.java
@@ -2,10 +2,16 @@ package com.dauphine.my_trip.repositories;
 
 import com.dauphine.my_trip.models.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CityRepository extends JpaRepository<City, UUID> {
+    @Query("SELECT c FROM City c WHERE UPPER(c.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    List<City> findByNameContainingIgnoreCase(@Param("name") String name);
+
     Optional<City> findByName(String cityName);
 }

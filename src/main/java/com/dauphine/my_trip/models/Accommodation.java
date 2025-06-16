4a148b8161988139fce43e02185185eb8e8521b9
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-16 19:34:02 +0200

    Create Accommodation.java
    Create the model for Accommodation

---
+++ src/main/java/com/dauphine/my_trip/models/Accommodation.java
@@ -0,0 +1,100 @@
+package com.dauphine.my_trip.models;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "accommodation")
+public class Accommodation {
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "address")
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public Accommodation() {}
+
+    public Accommodation(String name, String type, int rating, int price, String address, City city) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.type = type;
+        this.rating = rating;
+        this.price = price;
+        this.address = address;
+        this.city = city;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}

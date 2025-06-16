c035c23b7d4b62b41a5d9196d8229b10eacfc55d
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-16 19:34:38 +0200

    Create AccommodationRepository.java
    Create the repository for the Accommodation model

---
+++ src/main/java/com/dauphine/my_trip/repositories/AccommodationRepository.java
@@ -0,0 +1,17 @@
+package com.dauphine.my_trip.repositories;
+
+import com.dauphine.my_trip.models.Accommodation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AccommodationRepository extends JpaRepository<Accommodation, UUID> {
+    @Query("SELECT acc FROM Accommodation acc WHERE UPPER(acc.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    List<Accommodation> findByNameContainingIgnoreCase(@Param("name") String name);
+
+    Optional<Accommodation> findByName(String accommodationName);
+}

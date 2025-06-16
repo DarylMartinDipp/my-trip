3356c82b408027c734323fffaca7afa1be20ad5f
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-16 19:35:03 +0200

    Create Service for Accommodation
    Create the service and its implementation

---
+++ src/main/java/com/dauphine/my_trip/services/AccommodationService.java
@@ -0,0 +1,27 @@
+package com.dauphine.my_trip.services;
+
+import com.dauphine.my_trip.exceptions.accommodation.AccommodationNameAlreadyExistsException;
+import com.dauphine.my_trip.exceptions.accommodation.AccommodationNotFoundByIdException;
+import com.dauphine.my_trip.models.Accommodation;
+import com.dauphine.my_trip.models.City;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AccommodationService {
+
+    List<Accommodation> getAllAccommodations();
+
+    Accommodation getAccommodationById(UUID accommodationId) throws AccommodationNotFoundByIdException;
+
+    Optional<Accommodation> getAccommodationByName(String accommodationName);
+
+    List<Accommodation> getAccommodationByNameIgnoreCase(String accommodationName);
+
+    Accommodation createAccommodation(String newAccommodationName, String newAccommodationType, int newAccommodationRating, int newAccommodationPrice, String newAccommodationAddress, City newAccommodationCity) throws AccommodationNameAlreadyExistsException;
+
+    Accommodation updateAccommodation(UUID accommodationId, String newAccommodationName, String newAccommodationType, int newAccommodationRating, int newAccommodationPrice, String newAccommodationAddress, City newAccommodationCity) throws AccommodationNotFoundByIdException, AccommodationNameAlreadyExistsException;
+
+    void deleteAccommodation(UUID accommodationToDeleteId) throws AccommodationNotFoundByIdException;
+}
---
+++ src/main/java/com/dauphine/my_trip/services/impl/AccommodationServiceImpl.java
@@ -0,0 +1,75 @@
+package com.dauphine.my_trip.services.impl;
+
+import com.dauphine.my_trip.exceptions.accommodation.AccommodationNameAlreadyExistsException;
+import com.dauphine.my_trip.exceptions.accommodation.AccommodationNotFoundByIdException;
+import com.dauphine.my_trip.models.Accommodation;
+import com.dauphine.my_trip.models.City;
+import com.dauphine.my_trip.repositories.AccommodationRepository;
+import com.dauphine.my_trip.services.AccommodationService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AccommodationServiceImpl implements AccommodationService {
+    private final AccommodationRepository accommodationRepository;
+
+    public AccommodationServiceImpl(AccommodationRepository accommodationRepository) {
+        this.accommodationRepository = accommodationRepository;
+    }
+
+    @Override
+    public List<Accommodation> getAllAccommodations() {
+        return accommodationRepository.findAll();
+    }
+
+    @Override
+    public Accommodation getAccommodationById(UUID accommodationId) throws AccommodationNotFoundByIdException {
+        return accommodationRepository.findById(accommodationId).orElseThrow(() -> new AccommodationNotFoundByIdException(accommodationId));
+    }
+
+    @Override
+    public Optional<Accommodation> getAccommodationByName(String accommodationName) {
+        return accommodationRepository.findByName(accommodationName);
+    }
+
+    @Override
+    public List<Accommodation> getAccommodationByNameIgnoreCase(String accommodationName) {
+        return accommodationRepository.findByNameContainingIgnoreCase(accommodationName);
+    }
+
+    @Override
+    public Accommodation createAccommodation(String newAccommodationName, String newAccommodationType, int newAccommodationRating, int newAccommodationPrice, String newAccommodationAddress, City newAccommodationCity) throws AccommodationNameAlreadyExistsException {
+        Optional<Accommodation> existingAccommodationByName = getAccommodationByName(newAccommodationName);
+        if (existingAccommodationByName.isPresent()) throw new AccommodationNameAlreadyExistsException(newAccommodationName);
+
+        Accommodation newAccommodation = new Accommodation(newAccommodationName, newAccommodationType, newAccommodationRating, newAccommodationPrice, newAccommodationAddress, newAccommodationCity);
+        return accommodationRepository.save(newAccommodation);
+    }
+
+    @Override
+    public Accommodation updateAccommodation(UUID accommodationId, String newAccommodationName, String newAccommodationType, int newAccommodationRating, int newAccommodationPrice, String newAccommodationAddress, City newAccommodationCity) throws AccommodationNotFoundByIdException, AccommodationNameAlreadyExistsException {
+        Accommodation accommodationToUpdate = getAccommodationById(accommodationId);
+
+        Optional<Accommodation> existingAccommodationByName = getAccommodationByName(newAccommodationName);
+        if (existingAccommodationByName.isPresent() && !existingAccommodationByName.get().equals(accommodationToUpdate))
+            throw new AccommodationNameAlreadyExistsException(newAccommodationName);
+
+        accommodationToUpdate.setName(newAccommodationName);
+        accommodationToUpdate.setType(newAccommodationType);
+        accommodationToUpdate.setRating(newAccommodationRating);
+        accommodationToUpdate.setPrice(newAccommodationPrice);
+        accommodationToUpdate.setAddress(newAccommodationAddress);
+        accommodationToUpdate.setCity(newAccommodationCity);
+
+        return accommodationRepository.save(accommodationToUpdate);
+    }
+
+    @Override
+    public void deleteAccommodation(UUID accommodationToDeleteId) throws AccommodationNotFoundByIdException {
+        getAccommodationById(accommodationToDeleteId);
+        accommodationRepository.deleteById(accommodationToDeleteId);
+    }
+}

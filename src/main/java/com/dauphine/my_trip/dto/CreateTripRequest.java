b801dccbda3f4680400a2ad1dc4485baf0518708
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-20 17:32:39 +0200

    Create CreateTripRequest.java

---
+++ src/main/java/com/dauphine/my_trip/dto/CreateTripRequest.java
@@ -0,0 +1,39 @@
+package com.dauphine.my_trip.dto;
+
+import java.time.LocalDate;
+
+public class CreateTripRequest {
+    private String title;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public CreateTripRequest(String title, LocalDate startDate, LocalDate endDate) {
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}

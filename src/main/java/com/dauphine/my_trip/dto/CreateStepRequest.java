5e9384d166a712a74fd57f509b2665f03fbee599
DarylMartinDipp <79166147+DarylMartinDipp@users.noreply.github.com>
2025-06-20 19:20:03 +0200

    Update StepRequest - Add/Remove activities
    Modify the request in order to add the possibility to add and delete activities

--- src/main/java/com/dauphine/my_trip/dto/CreateStepRequest.java
+++ src/main/java/com/dauphine/my_trip/dto/CreateStepRequest.java
@@ -4,17 +4,22 @@ import com.dauphine.my_trip.models.Accommodation;
 import com.dauphine.my_trip.models.City;
 import com.dauphine.my_trip.models.Trip;
 
+import java.util.List;
+import java.util.UUID;
+
 public class CreateStepRequest {
     private int day;
     private City city;
     private Accommodation accommodation;
     private Trip trip;
+    private List<UUID> activityIds;
 
-    public CreateStepRequest(int day, City city, Accommodation accommodation, Trip trip) {
+    public CreateStepRequest(int day, City city, Accommodation accommodation, Trip trip, List<UUID> activityIds) {
         this.day = day;
         this.city = city;
         this.accommodation = accommodation;
         this.trip = trip;
+        this.activityIds = activityIds;
     }
 
     public int getDay() {
@@ -47,5 +52,13 @@ public class CreateStepRequest {
 
     public void setTrip(Trip trip) {
         this.trip = trip;
+    }
+
+    public List<UUID> getActivityIds() {
+        return activityIds;
+    }
+
+    public void setActivityIds(List<UUID> activityIds) {
+        this.activityIds = activityIds;
     }
 }
